c136d51421eef91b2fe91aeab0cdeadbc2480f56
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-19 02:41:25 +0100

    19/02 - 02.39
    End of Day Commit:
    Rough design of modal screen, only preparation list and YouTube video left.
    
    Next steps:
    Implementation of database
    additional media queries
    readme adjustments

--- mealstation/src/components/Recipe.js
+++ mealstation/src/components/Recipe.js
@@ -1,23 +1,35 @@
 import React from "react";
-import {useState} from 'react';
-import {useEffect} from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 import "../scss/components/recipe.scss";
 import flagItaly from "../assets/image/flags/italy.png";
 import flagArgentina from "../assets/image/flags/argentina.png";
 import flagChina from "../assets/image/flags/china.png";
 import flagUSA from "../assets/image/flags/usa.png";
+import cheesecake from "../assets/image/food/cheesecake.jpeg";
 
 const Recipe = (props) => {
   const [showModal, setModal] = useState(false);
 
-   useEffect(() => {
-     if (showModal) {
-       document.body.style.overflow = "hidden";
-     } else {
-       document.body.style.overflow = "auto";
-     }
-   }, [showModal]);
-  
+  useEffect(() => {
+    if (showModal) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showModal]);
+
+  useEffect(() => {
+    if (showModal) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+    return () => {
+      document.body.classList.remove("modal-open");
+    };
+  }, [showModal]);
+
   let flagSource;
 
   if (props.flag === "italy") {
@@ -40,8 +52,21 @@ const Recipe = (props) => {
       {showModal && (
         <div className="background">
           <div className="flex-container">
-            <p className="exit" onClick={() => setModal(false)}>EXIT</p>
-            
+            <p className="exit" onClick={() => setModal(false)}>
+              EXIT
+            </p>
+            <div className="info">
+              <h4 className="title">NEW YORK CHEESECAKE</h4>
+              <h5 className="type">APPETIZER</h5>
+              <img className="thumbflag" src={flagUSA} alt="american flag"></img>
+              <img className="thumb" src={cheesecake} alt="cheesecake"></img>
+              <h6 className="ingredients-title">INGREDIENTS</h6>
+              <ul>
+                <li>111111111111111111</li>
+                <li>222222222</li>
+              </ul>
+              <h6 className="preparation-title">PREPARATION</h6>
+            </div>
           </div>
         </div>
       )}
